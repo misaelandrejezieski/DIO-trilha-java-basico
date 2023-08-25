394a8a84810ea9948722e47d4b79fee7858fa7ca
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-08-25 16:29:31 -0300

    DES

--- BancoTerminal/src/BancoTerminal.java
+++ BancoTerminal/src/BancoTerminal.java
@@ -1,16 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class BancoTerminal {
     public static void main(String[] args) throws Exception {
-        double saldo = 25.0;
-        double valorSolicitado = 22.0;
-
+        double saldo = 110;
+        double valorSolicitado = 10;
         if(valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
-        System.out.println("\n## Valor solicitado para saque:" + valorSolicitado);
-        System.out.println("\n## Saldo restante: " + saldo);  
+                System.out.println("\n## Valor solicitado para saque:" + valorSolicitado);
+                System.out.println("\n## Saldo restante: " + saldo);  
         }else if (saldo < valorSolicitado){
-            System.out.println("Saldo insuficiente");  
+                System.out.println("Saldo insuficiente");  
         }
           
-        System.out.println("\nTenha um bom dia!!!");
+        System.out.println("\n-- Tenha um bom dia! --");
     }
 }
