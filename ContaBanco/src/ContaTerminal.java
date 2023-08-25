f5b289bf058476db70f964578109333872fff6a2
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-08-25 15:08:27 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -29,4 +29,73 @@ public class ContaTerminal {
 
     scan.close();
     }
+
+    public class BankAccount {
+        private String accountHolderName;
+        private int accountNumber;
+        private double balance;
+    
+        public BankAccount(String accountHolderName, int accountNumber, double balance) {
+            this.accountHolderName = accountHolderName;
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+        }
+    
+        public String getAccountHolderName() {
+            return accountHolderName;
+        }
+    
+        public void setAccountHolderName(String accountHolderName) {
+            this.accountHolderName = accountHolderName;
+        }
+    
+        public int getAccountNumber() {
+            return accountNumber;
+        }
+    
+        public void setAccountNumber(int accountNumber) {
+            this.accountNumber = accountNumber;
+        }
+    
+        public double getBalance() {
+            return balance;
+        }
+    
+        public void setBalance(double balance) {
+            this.balance = balance;
+        }
+    }
+    
+    public class ElectronicService {
+        private BankAccount bankAccount;
+    
+        public ElectronicService(BankAccount bankAccount) {
+            this.bankAccount = bankAccount;
+        }
+    
+        public void deposit(double amount) {
+            bankAccount.setBalance(bankAccount.getBalance() + amount);
+            System.out.println("Depósito bem-sucedido. Novo saldo: " + bankAccount.getBalance());
+        }
+    
+        public void withdraw(double amount) {
+            if (bankAccount.getBalance() >= amount) {
+                bankAccount.setBalance(bankAccount.getBalance() - amount);
+                System.out.println("Retirada bem-sucedida. Novo saldo: " + bankAccount.getBalance());
+            } else {
+                System.out.println("Saldo insuficiente.");
+            }
+        }
+    
+        public void transfer(double amount, BankAccount recipient) {
+            if (bankAccount.getBalance() >= amount) {
+                bankAccount.setBalance(bankAccount.getBalance() - amount);
+                recipient.setBalance(recipient.getBalance() + amount);
+                System.out.println("Transferência bem-sucedida. Novo saldo: " + bankAccount.getBalance());
+            } else {
+                System.out.println("Saldo insuficiente.");
+            }
+        }
+    }
+    
 }
