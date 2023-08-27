6da8a2a72f055f09ade0bcc2785ddb2c8367aab4
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-08-27 13:28:08 -0300

    erro de classe

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("### PROCESSO SELETIVO ###");
+
+
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido){
+            System.out.println("A sua candidatura foi aceite.");
+        } else if (salarioBase == salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO COMO CONTRA PROPOSTA.");
+        } else {
+            System.out.println("AGUARDAR DEMAIS CANDIDATOS.");
+        }
+
+
     }
 }
