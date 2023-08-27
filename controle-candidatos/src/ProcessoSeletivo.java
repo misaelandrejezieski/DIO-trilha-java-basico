0592c3f9c28a9390e2f2fe703201fa2bcf9636a4
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-08-27 15:07:54 -0300

    update001

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -1,26 +1,40 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
         System.out.println("### PROCESSO SELETIVO ###");
-        System.out.println("---- ---- ---- ---- ---- ---- ---- ---- ---- ----");
-        /*analisarCandidato(1900.0);
+        /*System.out.println("---- ---- ---- ---- ---- ---- ---- ---- ---- ----");
+        analisarCandidato(1900.0);
         analisarCandidato(2200.0);
-        analisarCandidato(2000.0);*/
-
-        System.out.println("---- ---- ---- ---- ---- ---- ---- ---- ---- ----");
-
-    }
-
-    static void selecaoCandidatos(){
-        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","FABRICIO","MIRELA","DANIELA"};
-
-        int candidatosSelecionados = 0;
-        int candidatosAtual = 0;
-        while(candidatosSelecionados < 5){
-            String candidato = candidatos[candidatosAtual];
-
-        }
+        analisarCandidato(2000.0);
+        System.out.println("---- ---- ---- ---- ---- ---- ---- ---- ---- ----");*/
 
     }
+		
+	static void selecaoCandiditos() {
+		double salarioBase = 2000.0;
+		String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+		int totalSelecionados = 0;
+		int proximoCandidato = 0; 
+		while(totalSelecionados <5 && proximoCandidato < candidatos.length) {
+			String candidato = candidatos[proximoCandidato++];
+			double salarioPretendido = salarioPretendido();
+			System.out.println("O candidato " + candidato + " solicitau este valor de salário  " + salarioPretendido);
+			if(salarioBase >= salarioPretendido) {
+				System.out.println("QUE PENA!! O candidato " + candidato + " NÃO foi selecionado ");
+				
+			}else {
+				System.out.println("LEGAL!! O candidato " + candidato + " foi selecionado ");
+				totalSelecionados++;
+			}
+			
+		}
+		System.out.println("Total de selecionados: " + totalSelecionados);
+		System.out.println("Total de consultados: " + proximoCandidato);
+	}
+	static double salarioPretendido() {
+	     return ThreadLocalRandom.current().nextDouble(1800, 2200);
+	}
 
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
