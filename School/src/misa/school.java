fadc4cd69ada37fb9da69d2fd96e3a77ec56c63e
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-08-28 12:34:05 -0300

    update

--- School/src/misa/school.java
+++ School/src/misa/school.java
@@ -1,28 +1,26 @@
 package misa;
 
 import java.util.Scanner;
-
-
-    public class school {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            // Pega o número de linhas e colunas da matriz
-            int linhas = scanner.nextInt();
-            int colunas = scanner.nextInt();
-            // Cria a matriz
-            int[][] matriz = new int[linhas][colunas];
-            // Preenche a matriz com os valores digitados pelo usuário
-            for (int i = 0; i < linhas; i++) {
-                for (int j = 0; j < colunas; j++) {
-                    matriz[i][j] = scanner.nextInt();
-                }
+public class school {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Pega o número de linhas e colunas da matriz
+        int linhas = scanner.nextInt();
+        int colunas = scanner.nextInt();
+        // Cria a matriz
+        int[][] matriz = new int[linhas][colunas];
+        // Preenche a matriz com os valores digitados pelo usuário
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                matriz[i][j] = scanner.nextInt();
             }
-            // Imprime a matriz
-            for (int i = 0; i < linhas; i++) {
-                for (int j = 0; j < colunas; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.println();
+        }
+        // Imprime a matriz
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
