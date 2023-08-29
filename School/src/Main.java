b30c63d5d0791749932aca58bd9b1a4e0258da69
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-08-29 16:20:43 -0300

    colunas

--- School/src/Main.java
+++ School/src/Main.java
@@ -13,9 +13,9 @@ public class Main {
            
             int[][] matriz = new int[linhas][colunas];
         // Preenche a matriz com os valores digitados pelo usuário
-        for (int i = 0; i < linhas; i++) {
-            System.out.println("Digite o número de linhas: " + (i++));
-            for (int j = 0; j < colunas; j++) {
+        for (int i = 1; i <= linhas; i++) {
+            System.out.println("Digite o número de linhas: " + i);
+            for (int j = 1; j <= colunas; j++) {
                 matriz[i][j] = scanner.nextInt();
             }
         }
