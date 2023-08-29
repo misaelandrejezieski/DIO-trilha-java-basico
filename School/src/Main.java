6ba7b3b60d5c066088de29a6462aac057405cedc
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-08-29 15:45:21 -0300

    try

---
+++ School/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        try 
+        (Scanner scanner = new Scanner(System.in)) {
+            // Pega o número de linhas e colunas da matriz
+            int linhas = scanner.nextInt();
+            int colunas = scanner.nextInt();
+            // Cria a matriz
+            int[][] matriz = new int[linhas][colunas];
+            // Preenche a matriz com os valores digitados pelo usuário
+            for (int i = 0; i < linhas; i++) {
+                for (int j = 0; j < colunas; j++) {
+                    matriz[i][j] = scanner.nextInt();
+                }
+            }
+            // Imprime a matriz
+            for (int i = 0; i < linhas; i++) {
+                for (int j = 0; j < colunas; j++) {
+                    System.out.print(matriz[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+    
+}
