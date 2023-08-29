67b815a39ba9dc6c3940b6b9d3d108e036ab6e93
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-08-29 16:12:56 -0300

    deserrando

--- School/src/Main.java
+++ School/src/Main.java
@@ -8,24 +8,24 @@ public class Main {
             int linhas = scanner.nextInt();
             System.out.println("Digite o número de colunas: " );
             int colunas = scanner.nextInt();
+            System.out.println("Digite o número para preencher o array: " );
             // Cria a matriz
            
             int[][] matriz = new int[linhas][colunas];
-            // Preenche a matriz com os valores digitados pelo usuário
-            for (int i = 0; i < linhas; i++) {
-                 System.out.println("Digite o número para preencher o array: " );
-                for (int j = 0; j < colunas; j++) {
-                    matriz[i][j] = scanner.nextInt();
-                }
+        // Preenche a matriz com os valores digitados pelo usuário
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                matriz[i][j] = scanner.nextInt();
             }
-            // Imprime a matriz
-            //System.out.println("Array do misa." );
-            
-            for (int i = 0; i < linhas; i++) {
-                for (int j = 0; j < colunas; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.println();
+        }
+        // Imprime a matriz
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
         }
     }
+    
 }
